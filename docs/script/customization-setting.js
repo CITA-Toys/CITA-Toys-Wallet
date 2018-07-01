7cb98a2551cba6972d8aafc61df5e402bfdef27a
Mine77 <haichaozhu77@gmail.com>
2018-07-01 22:59:32 +0800

    Update customization-setting.js

--- docs/script/customization-setting.js
+++ docs/script/customization-setting.js
@@ -4,7 +4,7 @@
 
 var customization = {
 
-    name: 'Nervos Network', // name of this document
+    name: 'Microscope', // name of this document
 
     homepage: `${default_language}/microscope.md`,// the home page, extension name '.md' cannot be omitted.
 }
