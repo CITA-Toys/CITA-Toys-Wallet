d56ed3ba74e0580c06e5ff8e609f2c0f88cbaf1a
Haichao <haichaozhu77@gmail.com>
2018-07-01 22:20:26 +0800

    add docs

---
+++ docs/script/customization-setting.js
@@ -0,0 +1,10 @@
+// This file is for you to customize.
+// It defines some of the customization settings.
+// settings in this file can overwrite the settings in the common-setting.js
+
+var customization = {
+
+    name: 'Nervos Network', // name of this document
+
+    homepage: `${default_language}/microscope.md`,// the home page, extension name '.md' cannot be omitted.
+}
